1e5b9459ac160705dc145a7ce599060ec2f60ef3
iftakharalamrizve <iftakharalam1100@gmail.com>
2022-12-12 17:56:33 +0600

    Package Infomraiton ready

--- tree-view/src/components/Tree.jsx
+++ tree-view/src/components/Tree.jsx
@@ -3,7 +3,7 @@ function Tree({ treeData, nodeLength }) {
     return (
         <ul id="tree" className="tree" >
             {treeData.map((node,index) => (
-                <TreeNode node={node} key={node.key} lastNode={index == nodeLength ? true : false} />
+                <TreeNode node={node} key={node.key} lastNode={index === nodeLength ? true : false} />
             ))}
         </ul>
     )
